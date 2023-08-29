7dfb01018f389763daa48b366d537d3290a48b99
LuiSouza <luisttim@gmail.com>
2023-08-29 09:29:36 -0300

    adding anotacao entity

--- src/main/java/br/gov/sp/fatec/springboot3app/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/springboot3app/entity/Usuario.java
@@ -1,6 +1,16 @@
 package br.gov.sp.fatec.springboot3app.entity;
 
-import jakarta.persistence;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "usr_usuario")
 public class Usuario {
@@ -15,6 +25,12 @@ public class Usuario {
 
     @Column(name = "usr_senha")
     private String senha;
+
+    @OneToMany(mappedBy = "usuario")
+    private Set<Anotacao> anotacoes;
+
+    @ManyToMany(mappedBy = "usuarios")
+    private Set<Autorizacao> autorizacoes;
 
     public Usuario(String nome, String senha) {
         this.nome = nome;
@@ -46,4 +62,21 @@ public class Usuario {
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
+    public Set<Anotacao> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(Set<Anotacao> anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+
+    public Set<Autorizacao> getAutorizacoes() {
+        return autorizacoes;
+    }
+
+    public void setAutorizacoes(Set<Autorizacao> autorizacoes) {
+        this.autorizacoes = autorizacoes;
+    }
+
 }
